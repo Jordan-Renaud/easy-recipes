d4fa370485c3766a7a6c44563e5f20b0c399d55b
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-06-22 15:26:08 +0100

    add styling to home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <button id="home"></button>
+        <button id="help"></button>
+      </header>
       <h1 className="title">Easy Recipes</h1>
       <h2 className="subheading">finding recipes doesn't have to be hard</h2>
     </div>
