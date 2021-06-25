b9ebe469b934f8a94cd98363f7e095a30b998c9e
Jordan Renaud <jordan.dj.renaud@gmail.com>
2021-06-25 09:59:11 +0100

    fix bug that stops recipe pages opening

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,38 +1,6 @@
 import "./Recipe.css";
 import { useParams } from "react-router";
-
-const recipes = {
-  "rice-bowl": {
-    title: "Rice Bowl",
-    proteins: ["chicken", "beef", "tofu", "edamame"],
-    vegetables: ["corn", "carrot", "peas", "cabbage", "raddish"],
-    starchs: ["basmati", "sushi rice", "quinoa"],
-    ingredients: ["soy sauce", "coriander", "brown sugar", "garlic"],
-    steps: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      "Ut non metus nec dolor vestibulum commodo",
-      "Ut commodo tellus non fringilla malesuada",
-      "Nullam ac ligula a dui dapibus tincidunt sit amet eu dolor",
-      "Morbi id neque eleifend, scelerisque eros ac, imperdiet neque",
-      "Nullam vulputate metus vel elit congue suscipit non sit amet neque",
-    ],
-  },
-  spaghetti: {
-    title: "Spaghetti",
-    proteins: ["chicken", "beef", "tofu", "edamame"],
-    vegetables: ["corn", "carrot", "peas", "cabbage", "raddish"],
-    starchs: ["basmati", "sushi rice", "quinoa"],
-    ingredients: ["soy sauce", "coriander", "brown sugar", "garlic"],
-    steps: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      "Ut non metus nec dolor vestibulum commodo",
-      "Ut commodo tellus non fringilla malesuada",
-      "Nullam ac ligula a dui dapibus tincidunt sit amet eu dolor",
-      "Morbi id neque eleifend, scelerisque eros ac, imperdiet neque",
-      "Nullam vulputate metus vel elit congue suscipit non sit amet neque",
-    ],
-  },
-};
+import { recipes } from "./data";
 
 export default function Recipe() {
   let { id } = useParams();
@@ -40,7 +8,7 @@ export default function Recipe() {
 
   return (
     <div className="Recipe">
-      <img src="https://picsum.photos/500/500" />
+      <img src={recipes[id].image} />
       <h1>{recipes[id].title}</h1>
     </div>
   );
